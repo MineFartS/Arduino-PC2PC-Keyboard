45a06c8d8a1f4a411d39c17f965bcb1965cc0161
Phil Hunt <philiphuntiv2@gmail.com>
2026-08-24 10:20:37 -0400

    Remap Keys in usb-keyboard.h
    https://www.sjoerdlangkemper.nl/2022/11/16/running-etherkey-on-arduino-leonardo/#:~:text=All%20in%20all,changes:

--- etherkey/usb-keyboard.h
+++ etherkey/usb-keyboard.h
@@ -1,7 +1,26 @@
 #pragma once
 
 #define HWSERIAL Serial1
+#include "Keyboard.h"
 #include "utils.h"
+
+#define KEY_UP          KEY_UP_ARROW
+#define KEY_DOWN        KEY_DOWN_ARROW
+#define KEY_RIGHT       KEY_RIGHT_ARROW
+#define KEY_LEFT        KEY_LEFT_ARROW
+
+#define KEYPAD_PLUS     KEY_KP_PLUS
+#define KEYPAD_0        KEY_KP_0
+
+#define KEY_ENTER       KEY_RETURN
+#define KEY_SPACE       ' '
+
+#define MODIFIERKEY_CTRL  KEY_LEFT_CTRL
+#define MODIFIERKEY_ALT   KEY_LEFT_ALT
+#define MODIFIERKEY_SHIFT KEY_LEFT_SHIFT
+#define MODIFIERKEY_GUI   KEY_LEFT_GUI
+
+#define keyboard_leds   0
 
 #define MYDEBUG
 #define KBD_BUFFSZ 200
