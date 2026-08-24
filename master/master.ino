8572171a7488564b52494012665f4f39bfdd156a
Phil Hunt <philiphuntiv2@gmail.com>
2026-08-24 12:33:16 -0400

    Update master.ino

--- master/master.ino
+++ master/master.ino
@@ -1,34 +1,32 @@
-#include <Arduino.h>
-#include <Keyboard.h>
 
-// Using Port 9 for the touch sensor
-const int touchPin = 9;
+// USB Virtual COM Port connected to your Client PC
+#define CLIENT_PORT Serial
+
+// Hardware UART Tx/Rx Pins wired to the Slave Node
+#define SLAVE_PORT  Serial1
+
+const uint32_t ETHERKEY_BAUD = 57600;
 
 void setup() {
-    
-    // INPUT_PULLUP keeps the pin HIGH until ground forces it LOW
-    pinMode(touchPin, INPUT_PULLUP);
-    
-    Keyboard.begin();
 
-}
+  // Initialize the native USB subsystem 
+  CLIENT_PORT.begin(ETHERKEY_BAUD);
+  
+  // Initialize physical TX (Pin 1) and RX (Pin 0) 
+  SLAVE_PORT.begin(ETHERKEY_BAUD);
 
-bool is_touched() {
-    return digitalRead(touchPin) == LOW;
 }
 
 void loop() {
 
-    if (is_touched()) {
+    // Pipe: Client PC -> Slave Node
+    while (CLIENT_PORT.available() > 0) {
+        SLAVE_PORT.write(CLIENT_PORT.read());
+    }
 
-        // Press the Enter key
-        Keyboard.write(KEY_RETURN);
-
-        // Block execution until the port is released from ground
-        while (is_touched()) {
-            delay(10);
-        }
-
+    // Pipe: Slave Node -> Client PC
+    while (SLAVE_PORT.available() > 0) {
+        CLIENT_PORT.write(SLAVE_PORT.read());
     }
 
 }
